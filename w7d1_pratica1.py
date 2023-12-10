36cd8d2ac48f200d1da1968088de631373d20a13
Drerocca <148903780+Drerocca@users.noreply.github.com>
2023-12-10 14:14:51 +0100

    Create w7d1_pratica1.py

---
+++ w7d1_pratica1.py
@@ -0,0 +1,10 @@
+lista_a = ["ciao", "grazie", "sole", "libro", "pizza"]
+lista_b = []
+
+print("Lista A:", lista_a)
+
+for parola in lista_a:
+    lunghezza_parola = len(parola)
+    lista_b.append(lunghezza_parola)
+
+print("Lista B:", lista_b)
